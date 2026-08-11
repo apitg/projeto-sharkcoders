0c3bf1b3b5ef85a7299932694be9cdbf976093c5
ap1tg <afonsopittagarcia@gmail.com>
2026-08-11 14:08:30 +0100

    Update username in authentication prompt and welcome message

--- perguntas.py
+++ perguntas.py
@@ -1,9 +1,9 @@
 username = input("Qual é o nome de utilizador?: ") # pergunta o nome de utilizador
 
-if username == "afonso": # se o nome de utilizador for afonso, então pede a password
+if username == "abc": # se o nome de utilizador for abc, então pede a password
     password = input("Qual é a password?: ") # pede a password
     if password == "1234": # se a password for 1234, então...
-        print("Bem-vindo, afonso!") # dar boas-vindas
+        print("Bem-vindo, abc!") # dar boas-vindas
     else:
         print("Password incorreta!") # senão, diz que a password está incorreta
 else:
